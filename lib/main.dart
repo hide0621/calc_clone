4af98a7cc73b226320d82d09b7a40dae9d695a10
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-08-22 09:24:11 +0900

    テストの作成のために必要なコードを用意して、計算機アプリを完成させた

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,77 @@
+import 'package:calc_clone/widgets/button.dart';
+import 'package:calc_clone/widgets/keypad.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'constants.dart';
+import 'logic.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
+  MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String txtResult = '0';
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final Logic _logic = Logic();
 
-  @override
   Widget build(BuildContext context) {
+    final FunctionOnPressed onPressed = (String text) {
+      _logic.input(text);
+      setState(() {
+        txtResult = _logic.text;
+      });
+    };
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
+      backgroundColor: colorMain,
+      body: Container(
+        margin: const EdgeInsets.only(bottom: 20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    txtResult,
+                    key: const Key('txtResult'),
+                    style: const TextStyle(
+                      color: colorText,
+                      fontSize: 60,
+                    ),
+                    textAlign: TextAlign.right,
+                    maxLines: 1,
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            KeyPad(onPressed),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
-}
+} // end of state class
