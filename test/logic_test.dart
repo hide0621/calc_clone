acf5aa46a5d654dc8c78d677718a26650b96638e
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-08-23 09:29:33 +0900

    lateinitする理由とsetUp関数についてドキュメントコメントを書いた

--- test/logic_test.dart
+++ test/logic_test.dart
@@ -2,9 +2,13 @@ import 'package:calc_clone/logic.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  /// ここではまだ初期化しない
   late Logic logic;
 
+  /// setUp関数は、Dartのflutter_testパッケージで提供される関数で、
+  /// 各テストが実行される前に必ず呼び出されるように設計されている
   setUp(() {
+    /// ここで初期化
     logic = Logic();
   });
 
