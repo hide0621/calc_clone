5eab3348807f53949a273569358d8b932ce7fbe4
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-08-23 09:22:47 +0900

    テスト対象のオブジェクト生成が冗長化していたので、setUp関数内でオブジェクト化するように修正した

--- test/logic_test.dart
+++ test/logic_test.dart
@@ -2,20 +2,26 @@ import 'package:calc_clone/logic.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  late Logic logic;
+
+  setUp(() {
+    logic = Logic();
+  });
+
   test("1をそのまま出力", () {
-    final logic = Logic();
+    expect(logic.text, '0');
     logic.input('1');
     expect(logic.text, '1');
   });
 
   test("2をそのまま出力", () {
-    final logic = Logic();
+    expect(logic.text, '0');
     logic.input('2');
     expect(logic.text, '2');
   });
 
   test("3をそのまま出力", () {
-    final logic = Logic();
+    expect(logic.text, '0');
     logic.input('3');
     expect(logic.text, '3');
   });
