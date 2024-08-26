e9beb7056eda2b7a518f4027040b41ec4029406f
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-08-26 09:28:44 +0900

    小数点の入力に対応していることを示すテストコードを書いてみた

--- test/logic_test.dart
+++ test/logic_test.dart
@@ -51,4 +51,28 @@ void main() {
     logic.input('9');
     expect(logic.text, '123,456,789');
   });
+
+  test("小数点の入力", () {
+    expect(logic.text, '0');
+    logic.input('1');
+    expect(logic.text, '1');
+    logic.input('.');
+    expect(logic.text, '1.');
+    logic.input('2');
+    expect(logic.text, '1.2');
+    logic.input('3');
+    expect(logic.text, '1.23');
+    logic.input('4');
+    expect(logic.text, '1.234');
+    logic.input('5');
+    expect(logic.text, '1.2345');
+    logic.input('6');
+    expect(logic.text, '1.23456');
+    logic.input('7');
+    expect(logic.text, '1.234567');
+    logic.input('8');
+    expect(logic.text, '1.2345678');
+    logic.input('9');
+    expect(logic.text, '1.23456789');
+  });
 }
