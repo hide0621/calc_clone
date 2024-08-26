659cc4c41dd64754f310b143c16508ebf5537292
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-08-26 09:14:52 +0900

    intlパッケージを使えば小数点と３桁区切りにも対応できることを示すテストコードを書いてみた

--- test/intl_test.dart
+++ test/intl_test.dart
@@ -9,4 +9,27 @@ void main() {
     expect(formatter.format(1234567), '1,234,567');
     expect(formatter.format(123456789), '123,456,789');
   });
+
+  test("小数点", () {
+    intl.NumberFormat formatter = intl.NumberFormat('#,###.########', 'en_US');
+
+    /// 小数点はパス
+    expect(formatter.format(123.456), '123.456');
+    expect(formatter.format(12.34), '12.34');
+    expect(formatter.format(1234.567), '1,234.567');
+    expect(formatter.format(1.23456789), '1.23456789');
+
+    /// 以下のコードは仕様上パスしない
+    // expect(formatter.format(0.0), '0.0');
+    // expect(formatter.format(0.00000000), '0.00000000');
+
+    /// ただし、これはパスする
+    expect(formatter.format(0.00000001), '0.00000001');
+
+    /// 小数点 + ３桁区切りもパス
+    expect(formatter.format(123), '123');
+    expect(formatter.format(1234), '1,234');
+    expect(formatter.format(1234567), '1,234,567');
+    expect(formatter.format(123456789), '123,456,789');
+  });
 }
