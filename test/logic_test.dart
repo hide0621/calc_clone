d05e581144208c80076250ebd646704b22aae310
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-08-30 08:45:42 +0900

    整数と小数のロジック部分のテストをgroup化してみた

--- test/logic_test.dart
+++ test/logic_test.dart
@@ -12,67 +12,71 @@ void main() {
     logic = Logic();
   });
 
-  test("1をそのまま出力", () {
-    expect(logic.text, '0');
-    logic.input('1');
-    expect(logic.text, '1');
+  group("整数", () {
+    test("1をそのまま出力", () {
+      expect(logic.text, '0');
+      logic.input('1');
+      expect(logic.text, '1');
+    });
+
+    test("2をそのまま出力", () {
+      expect(logic.text, '0');
+      logic.input('2');
+      expect(logic.text, '2');
+    });
+
+    test("3をそのまま出力", () {
+      expect(logic.text, '0');
+      logic.input('3');
+      expect(logic.text, '3');
+    });
+
+    test("連続でテスト", () {
+      expect(logic.text, '0');
+      logic.input('1');
+      expect(logic.text, '1');
+      logic.input('2');
+      expect(logic.text, '12');
+      logic.input('3');
+      expect(logic.text, '123');
+      logic.input('4');
+      expect(logic.text, '1,234');
+      logic.input('5');
+      expect(logic.text, '12,345');
+      logic.input('6');
+      expect(logic.text, '123,456');
+      logic.input('7');
+      expect(logic.text, '1,234,567');
+      logic.input('8');
+      expect(logic.text, '12,345,678');
+      logic.input('9');
+      expect(logic.text, '123,456,789');
+    });
   });
 
-  test("2をそのまま出力", () {
-    expect(logic.text, '0');
-    logic.input('2');
-    expect(logic.text, '2');
-  });
-
-  test("3をそのまま出力", () {
-    expect(logic.text, '0');
-    logic.input('3');
-    expect(logic.text, '3');
-  });
-
-  test("連続でテスト", () {
-    expect(logic.text, '0');
-    logic.input('1');
-    expect(logic.text, '1');
-    logic.input('2');
-    expect(logic.text, '12');
-    logic.input('3');
-    expect(logic.text, '123');
-    logic.input('4');
-    expect(logic.text, '1,234');
-    logic.input('5');
-    expect(logic.text, '12,345');
-    logic.input('6');
-    expect(logic.text, '123,456');
-    logic.input('7');
-    expect(logic.text, '1,234,567');
-    logic.input('8');
-    expect(logic.text, '12,345,678');
-    logic.input('9');
-    expect(logic.text, '123,456,789');
-  });
-
-  test("小数点の入力", () {
-    expect(logic.text, '0');
-    logic.input('1');
-    expect(logic.text, '1');
-    logic.input('.');
-    expect(logic.text, '1.');
-    logic.input('2');
-    expect(logic.text, '1.2');
-    logic.input('3');
-    expect(logic.text, '1.23');
-    logic.input('4');
-    expect(logic.text, '1.234');
-    logic.input('5');
-    expect(logic.text, '1.2345');
-    logic.input('6');
-    expect(logic.text, '1.23456');
-    logic.input('7');
-    expect(logic.text, '1.234567');
-    logic.input('8');
-    expect(logic.text, '1.2345678');
-    logic.input('9');
-    expect(logic.text, '1.23456789');
+  group("小数", () {
+    test("小数点の入力", () {
+      expect(logic.text, '0');
+      logic.input('1');
+      expect(logic.text, '1');
+      logic.input('.');
+      expect(logic.text, '1.');
+      logic.input('2');
+      expect(logic.text, '1.2');
+      logic.input('3');
+      expect(logic.text, '1.23');
+      logic.input('4');
+      expect(logic.text, '1.234');
+      logic.input('5');
+      expect(logic.text, '1.2345');
+      logic.input('6');
+      expect(logic.text, '1.23456');
+      logic.input('7');
+      expect(logic.text, '1.234567');
+      logic.input('8');
+      expect(logic.text, '1.2345678');
+      logic.input('9');
+      expect(logic.text, '1.23456789');
+    });
   });
 }
