f1f022b16a95697ef93031f56b5b404367b9bf7b
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-09-27 10:13:39 +0900

    KeyPadウィジェットに対して「アイコン」が表示されているかを検証するウィジェットテストを実装

--- test/widgets/keypad_test.dart
+++ test/widgets/keypad_test.dart
@@ -1,4 +1,5 @@
 import 'package:calc_clone/widgets/keypad.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -31,9 +32,29 @@ void main() {
       expect(find.text('.'), findsOneWidget);
 
       /// 演算子は文字ではないので、テキストが存在しないことを確認
-      /// 演算子やパーセントなどは「アイコン」のテストで確認する
+      /// 演算子やパーセントなどは後述の「アイコン」のテストで確認する
       expect(find.text('+'), findsNothing);
       expect(find.text('='), findsNothing);
+    });
+
+    testWidgets('アイコン', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: KeyPad(null),
+        ),
+      );
+
+      await tester.pump();
+
+      /// キーパッド上にアイコンが表示されているか確認
+      /// アイコンはCupertinoIconsを使用しているため、Cupertinoのアイコンが表示される（iOS用のアプリなので）
+      expect(find.byIcon(CupertinoIcons.plus_slash_minus), findsOneWidget);
+      expect(find.byIcon(CupertinoIcons.percent), findsOneWidget);
+      expect(find.byIcon(CupertinoIcons.divide), findsOneWidget);
+      expect(find.byIcon(CupertinoIcons.multiply), findsOneWidget);
+      expect(find.byIcon(CupertinoIcons.minus), findsOneWidget);
+      expect(find.byIcon(CupertinoIcons.plus), findsOneWidget);
+      expect(find.byIcon(CupertinoIcons.equal), findsOneWidget);
     });
   });
 }
