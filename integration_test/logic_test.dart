e6a5c24c6d8f9a55796f6bcf5d534988ae115272
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-10-03 09:29:28 +0900

    数字がちゃんとテキストとして表示されているかをインテグレーションテストした

--- integration_test/logic_test.dart
+++ integration_test/logic_test.dart
@@ -1,5 +1,23 @@
+import 'package:calc_clone/main.dart' as app;
+import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group("initialized", () {
+    testWidgets("number", (tester) async {
+      /// インテグレーションテスト対象のアプリを起動
+      app.main();
+
+      /// 上記のアプリが起動するまで待機（[await]は必須）
+      await tester.pumpAndSettle();
+
+      expect(find.text(0.toString()), findsNWidgets(2));
+
+      for (int i = 1; i < 10; i++) {
+        expect(find.text(i.toString()), findsOneWidget);
+      }
+    });
+  });
 }
